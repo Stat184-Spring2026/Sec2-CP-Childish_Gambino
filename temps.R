4a92eb06b1c7d831141b0f8b6067079886229f88
Qah5033 <quinnhughes90@gmail.com>
2026-04-29 13:40:20 -0400

    wrangle of data
    wrangle and format the philly and state college data for use

--- temps.R
+++ temps.R
@@ -2,7 +2,26 @@ library(tidyverse)
 library(knitr)
 
 philadelphia_weather <- read_csv("C:\\Users\\quinn\\OneDrive - The Pennsylvania State University\\Yr2\\Stat184\\GitHub\\Sec2-CP-Childish_Gambino\\PhillyData.csv") |>
-  mutate(place = "Philadelphia")
+  mutate(
+    date = as.Date(DATE),
+    year = format(date, "%Y"),
+    month = as.Date(format(date, "%Y-%m-01")),
+    tmax = as.numeric(TMAX),
+    tmin = as.numeric(TMIN),
+    tobs = as.numeric(TOBS),
+    avg_temp = (tmax + tmin) / 2
+  ) |>
+  select(date, year, month, tmax, tmin, tobs, avg_temp)
 
 state_college_weather <- read_csv("C:\\Users\\quinn\\OneDrive - The Pennsylvania State University\\Yr2\\Stat184\\GitHub\\Sec2-CP-Childish_Gambino\\StateCollegeData.csv") |>
-  mutate(place = "State College")
+  mutate(
+    date = as.Date(DATE),
+    year = format(date, "%Y"),
+    month = as.Date(format(date, "%Y-%m-01")),
+    tmax = as.numeric(TMAX),
+    tmin = as.numeric(TMIN),
+    tobs = as.numeric(TOBS),
+    avg_temp = (tmax + tmin) / 2
+  ) |>
+  select(date, year, month, tmax, tmin, tobs, avg_temp)
+
