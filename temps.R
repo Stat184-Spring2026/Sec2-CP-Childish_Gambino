f71737a87ac76388087fb41b7d2456ada9c946dc
Qah5033 <quinnhughes90@gmail.com>
2026-04-29 13:29:34 -0400

    Update temps.R
    fix to file paths

--- temps.R
+++ temps.R
@@ -1,9 +1,8 @@
 library(tidyverse)
-library(lubridate)
 library(knitr)
 
-philadelphia_weather <- read_csv("philadelphia_weather.csv") |>
+philadelphia_weather <- read_csv("C:\\Users\\quinn\\OneDrive - The Pennsylvania State University\\Yr2\\Stat184\\GitHub\\Sec2-CP-Childish_Gambino\\PhillyData.csv") |>
   mutate(place = "Philadelphia")
 
-state_college_weather <- read_csv("state_college_weather.csv") |>
+state_college_weather <- read_csv("C:\\Users\\quinn\\OneDrive - The Pennsylvania State University\\Yr2\\Stat184\\GitHub\\Sec2-CP-Childish_Gambino\\StateCollegeData.csv") |>
   mutate(place = "State College")
